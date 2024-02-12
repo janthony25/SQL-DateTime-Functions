647adda97dd27dbecd12fddd95cc4efcca7e80a6
Jun Anthony <151007316+janthony25@users.noreply.github.com>
2024-02-12 22:58:34 +1300

    Update DateTime Functions.sql

--- DateTime Functions.sql
+++ DateTime Functions.sql
@@ -5,3 +5,8 @@ SELECT GETDATE()
 SELECT ISDATE('2020-01-23')  -- RETURN 1 if the expression is valid
 
 SELECT DAY('2020-01-23')  -- returns the Day || RESULT: 23
+
+SELECT MONTH('2020-01-23')  -- returns the MONTH || RESULT: 1
+
+  
+SELECT YEAR('2020-01-23') -- returns the YEAR || RESULT: 2020
