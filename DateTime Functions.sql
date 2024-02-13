ef810bd60761332d341f56f252086e32d3154591
Jun Anthony <151007316+janthony25@users.noreply.github.com>
2024-02-13 18:28:14 +1300

    Update DateTime Functions.sql

--- DateTime Functions.sql
+++ DateTime Functions.sql
@@ -10,3 +10,9 @@ SELECT MONTH('2020-01-23')  -- returns the MONTH || RESULT: 1
 
   
 SELECT YEAR('2020-01-23') -- returns the YEAR || RESULT: 2020
+
+--Datepart()
+SELECT DATEPART(Minute, '2020-01-20 04:55:12')  --Returns Integer
+
+--DateName()
+SELECT DATENAME(MONTH, '2020-01-20 04:55:12' )  -- Returns String(Nvarchar)
